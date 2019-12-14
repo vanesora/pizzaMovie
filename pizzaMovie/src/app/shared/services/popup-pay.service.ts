ab516a229d007d648bfb0d57bc54c14fc274ede0
dmoyanogonz <dmoyanogonz@hotmail.com>
2019-12-14 04:41:48 -0500

    Componente para pagos, usa un servicio falso para PSE y una implmentación de strip para TC

---
+++ pizzaMovie/src/app/shared/services/popup-pay.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { MatDialogRef, MatDialog, MatDialogConfig } from '@angular/material';
+import { PayComponent } from '../components/pay/pay.component';
+import { BuypizzaComponent } from '../components/pay/buypizza.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PopupPayService {
+
+  constructor(
+    private dialog: MatDialog,
+    private dialogBuyPizza: MatDialog,
+    private dialogRef: MatDialogRef<PayComponent>,
+    private dialogRefBuypizza: MatDialogRef<BuypizzaComponent>
+  ) { }
+
+  //Opens a dialog window, with payment info
+  //the type defines if is for pizza or for premium plan
+  openAlertDialog(type) {
+    console.log(type);
+    if (type == 'Pizza') {
+      this.dialogRefBuypizza = this.dialogBuyPizza.open(BuypizzaComponent);
+    } else {
+      this.dialogRef = this.dialog.open(PayComponent);
+    }
+  }
+}
