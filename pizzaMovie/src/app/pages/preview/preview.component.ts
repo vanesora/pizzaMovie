dc9e486b5bc2d1c5f696e7c9fa1cd98a60471dc4
laura03osa <57159470+laura03osa@users.noreply.github.com>
2019-12-19 18:40:23 -0500

    primera pagina

--- pizzaMovie/src/app/pages/preview/preview.component.ts
+++ pizzaMovie/src/app/pages/preview/preview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from 'src/app/shared/services/storage.service';
 
 @Component({
   selector: 'app-preview',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    public storageService: StorageService,
+  ) { }
 
   ngOnInit() {
   }
 
+  registro() {
+    this.storageService.setValue('page', 'Register')
+    this.router.navigate(['/register']);
+  }
 }
