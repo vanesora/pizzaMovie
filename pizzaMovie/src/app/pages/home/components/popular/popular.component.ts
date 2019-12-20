f0a081f9128017cd7df683f036672f64c80599ef
jenrod99 <jenrod_89@hotmail.com>
2019-12-20 00:55:04 -0500

    popularComponent -> Top Películas

--- pizzaMovie/src/app/pages/home/components/popular/popular.component.ts
+++ pizzaMovie/src/app/pages/home/components/popular/popular.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from 'src/app/shared/services/storage.service';
 import { MovieService } from '../../../../shared/services/movie.service';
+import { findReadVarNames } from '@angular/compiler/src/output/output_ast';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-popular',
@@ -12,28 +14,28 @@ export class PopularComponent implements OnInit {
 
   public details = document.getElementById('details')
   arrTopMovies;
+  peliculasUno;
+  peliculasDos;
+  peliculasTres
   constructor(
     public storageService: StorageService,
     public movieService: MovieService
-  ) { 
-    
+  ) {
+
   }
 
   ngOnInit() {
     this.topMovies();
-//     $http.get('GET-TOP-MOVIES')
-// .success(function(respuesta){
-// //código en caso de éxito
-// movies = RESPUESTA;
-// });
   }
 
   topMovies() {
-    var movies = this.storageService.movies;   
+    var movies = this.storageService.movies;
     this.movieService.getTopMovies().then(datos => {
       this.arrTopMovies = datos.movies;
+      this.peliculasUno = this.arrTopMovies.slice(0, 3),
+      this.peliculasDos = this.arrTopMovies.slice(3, 6),
+      this.peliculasTres = this.arrTopMovies.slice(6, 9)
     });
   }
-
 }
 
